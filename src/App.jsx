0f3ab2821d1a6f9ad8b90724b581d983a7c30e4c
oğuz kağan <36485087+ouzkagan@users.noreply.github.com>
2022-09-06 21:39:54 +0300

    chakra ui added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { fetchUsers } from "./features/user/userSlice";
 import { ordered, restocked } from "./features/cake/cakeSlice";
 import TodoList from "./features/todos/TodoList";
 import { Routes, Route } from "react-router-dom";
-import { Heading, VStack } from "@chakra-ui/react";
-
+import { Heading, VStack, IconButton } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
 function App() {
   // const user = useSelector((state) => state.user)
   // const numOfCakes = useSelector((state) => state.cake.numOfCakes)
@@ -18,32 +18,20 @@ function App() {
   // }, [])
 
   return (
-    <VStack>
-      <Heading>My Todo App</Heading>
+    <VStack p={4}>
+      <IconButton
+        borderRadius='3px'
+        size="lg"
+        alignSelf="flex-end"
+        icon = {colorMode === 'light' ? <FaMoon /> : <FaSun />}
+        ariaLabel= 'Switch DarkMode'
+      />
+      <Heading mg='8' fontWeight="extrabold" size="2xl">My Todo App</Heading>
 
       <Routes>
         <Route path="*" element={<TodoList />} />
       </Routes>
     </VStack>
-    
-      {/* number of cakes {numOfCakes} */}
-      {/* <button onClick={() => dispatch(ordered())}>order cake</button>
-      <button onClick={() => dispatch(restocked(5))}>restock cake</button>
-      <button onClick={() => dispatch(fetchUsers())}>get users</button>
-      <div>
-        {user.loading && <div>Users are loading</div>}
-        {!user.loading && user.error ? <div>error {user.error}</div> :null }
-        {!user.loading && user.users.length ? (
-            user.users.map(user => (
-              <li key={user.id}>{user.name}</li>
-            )
-
-          )
-        ) 
-        : null
-      }
-
-      </div> */}
   );
 }
 
--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -5,12 +5,23 @@ import {
 } from "../api/apiSlice";
 import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import TodoDetail from './TodoDetail'
-import AddTodo from './AddTodo'
-
+import TodoDetail from "./TodoDetail";
+import AddTodo from "./AddTodo";
+import {
+  HStack,
+  VStack,
+  Text,
+  IconButton,
+  StackDivider,
+  Spacer,
+  Badge,
+  Checkbox,
+  CheckboxGroup
+} from "@chakra-ui/react";
+import { FaTrash, FaEdit } from "react-icons/fa";
 
 export default function TodoList() {
- const navigate = useNavigate()
+  const navigate = useNavigate();
   const {
     data: todos,
     isLoading,
@@ -18,62 +29,85 @@ export default function TodoList() {
     isError,
     error,
   } = useGetTodosQuery();
-  
+
   const [updateTodo, { isLoading: isUpdating }] = useUpdateTodoMutation();
   const [deleteTodo] = useDeleteTodoMutation();
-
- 
 
   let content;
   if (isLoading) {
     content = <p>Loading..</p>;
   } else if (isSuccess) {
-    content = todos.map((todo) => {
-      return (
-        <article key={todo.id}>
-          <div className="todo">
-            <input
-              type="checkbox"
-              checked={todo.isCompleted}
-              id={todo.id}
-              onChange={() =>
-                updateTodo({ ...todo, isCompleted: !todo.isCompleted })
-              }
-            />
-            <label htmlFor={todo.id}>{todo.content}</label>
-            <span>
-              <button
-                className="trash"
-                onClick={() => navigate(`/todos/${todo.id}`)}
-              >
-                Edit
-              </button>
-            </span>
-            <span>
-              <button
-                className="trash"
-                onClick={() => deleteTodo({ id: todo.id })}
-              >
-                Delete
-              </button>
-            </span>
-          </div>
-        </article>
+    content =
+      todos.length > 0 ? (
+        <VStack
+          divider={<StackDivider />}
+          borderColor="gray.100"
+          borderWidth="2px"
+          p="4"
+          borderRadius="lg"
+          w="100%"
+          maxW={{ base: "90vw", sm: "80vw", lg: "50vw", xl: "40vw" }}
+          alignItems="stretch"
+        >
+          {todos.map((todo) => {
+            return (
+              <HStack key={todo.id}>
+                  {/* <input
+                    type="checkbox"
+                    checked={todo.isCompleted}
+                    id={todo.id}
+                    onChange={() =>
+                      updateTodo({ ...todo, isCompleted: !todo.isCompleted })
+                    }
+                  /> */}
+                  {/* <Checkbox
+                    defaultChecked={todo.isCompleted}
+                    id={todo.id}
+                    onChange={() =>
+                      updateTodo({ ...todo, isCompleted: !todo.isCompleted })
+                    }
+                  /> */}
+                  <div className={todo.isCompleted ? 'done' : ''}>
+                    <Checkbox colorScheme={todo.isCompleted ? 'gray' : ''} 
+                      defaultChecked={todo.isCompleted}
+                      id={todo.id}
+                      onChange={() =>
+                        updateTodo({ ...todo, isCompleted: !todo.isCompleted })
+                      } />
+                   </div>
+                  <Text>{todo.content}</Text>
+                  <Spacer />
+                  <IconButton
+                    icon={<FaEdit />}
+                    borderRadius="3px"
+                    onClick={() => navigate(`/todos/${todo.id}`)}
+                  />
+                  <IconButton
+                    icon={<FaTrash />}
+                    borderRadius="3px"
+                    onClick={() => deleteTodo({ id: todo.id })}
+                    
+                  />
+              </HStack>
+            );
+          })}
+        </VStack>
+      ) : (
+        <Badge colorScheme="green" p="4" m="4" borderRadius="lg">
+          No todos, yet.
+        </Badge>
       );
-    });
   } else if (isError) {
     content = <p>{error}</p>;
   }
   return (
     <main>
-      <AddTodo/>
+      <AddTodo />
       {isUpdating ? "updating please wait..." : ""}
       {content}
       <div>
         <Routes>
-          <Route path="/todos/:id" element={
-            <TodoDetail/>
-          } />
+          <Route path="/todos/:id" element={<TodoDetail />} />
           <Route
             element={
               <div>
